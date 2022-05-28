054f7db489c3759a8da18182660d7b9c7b3807a9
Maia Dutta <maiadutta@Maias-MBP.attlocal.net>
2022-05-28 15:58:42 -0500

    fixed form, and minor changes

--- script/main.js
+++ script/main.js
@@ -7,13 +7,19 @@
 //  console.log(fullName)
 //   // alert("Hello!")
 // });
-let submitButton = document.getElementById("submit-button")
+let submitButton = document.getElementById("submit-button");
 function handleClick() {
   let fullName = document.getElementById("fullName").value;
   let email = document.getElementById("email").value;
   let textBox = document.getElementById("textBox").value;
-  const message = `Hello, ${fullName}, we received your email address (${email}) and your comment (${textBox}).`
-  alert(message);
+  let message = "";
+  if (fullName !== "" && email !== "" && textBox !== "") {
+    message = `Hello, ${fullName}, we received your email address (${email}) and your comment (${textBox}).`;
+    alert(message);
+  } else if (fullName !== "" && email !== "" && textBox === "") {
+    message = `Hello, ${fullName}, we received your email address (${email}).`;
+    alert(message);
+  }
 }
 
-submitButton.addEventListener('click',handleClick);
+submitButton.addEventListener("click", handleClick);
