30d105db3a6964c9d98cab68c43b0a643f08d4e4
Maia Dutta <maiadutta@Maias-MBP.attlocal.net>
2022-05-31 21:53:20 -0500

    Formatted and added new images to places I've visited. Added favicon to all pages.

--- script/get-places.js
+++ script/get-places.js
@@ -2,10 +2,12 @@ const destinations = [
   "https://cdn.pixabay.com/photo/2021/04/06/11/22/hawa-mahal-6156123_960_720.jpg",
   "https://cdn.pixabay.com/photo/2016/11/02/16/02/natural-1792047_960_720.jpg",
   "https://cdn.pixabay.com/photo/2017/05/30/04/36/volcano-2355772_960_720.jpg",
-  "https://cdn.pixabay.com/photo/2022/04/02/14/57/whale-7107172_960_720.jpg"
+  "https://cdn.pixabay.com/photo/2020/12/07/22/36/boats-5813005_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/08/13/07/20/niagara-1590345_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2020/02/26/00/05/arizona-4880324_960_720.jpg",
 ]
 
-const names =[ "India", "Thailand", "Costa Rica", "Vancouver Island"]
+const names =[ "India", "Thailand", "Costa Rica", "Vancouver Island, BC", "Niagara Falls, NY", "Sedona, AZ",]
 
 const seePlacesButton = document.getElementById("seeplaces")
 
