117f984c0fa488246e66f0893ef8334a9d5820f8
Maia Dutta <mermaia33@gmail.com>
2022-06-09 15:42:36 -0500

    Changed spacing and font colors on resume page.

--- script/get-places.js
+++ script/get-places.js
@@ -5,39 +5,45 @@ const destinations = [
   "https://cdn.pixabay.com/photo/2020/12/07/22/36/boats-5813005_960_720.jpg",
   "https://cdn.pixabay.com/photo/2016/08/13/07/20/niagara-1590345_960_720.jpg",
   "https://cdn.pixabay.com/photo/2020/02/26/00/05/arizona-4880324_960_720.jpg",
-]
+];
 
-const names =[ "India", "Thailand", "Costa Rica", "Vancouver Island, BC", "Niagara Falls, NY", "Sedona, AZ",]
+const names = [
+  "India",
+  "Thailand",
+  "Costa Rica",
+  "Vancouver Island, BC",
+  "Niagara Falls, NY",
+  "Sedona, AZ",
+];
 
-const seePlacesButton = document.getElementById("seeplaces")
+const seePlacesButton = document.getElementById("seeplaces");
 
-const holder = document.getElementById("holder")
+const holder = document.getElementById("holder");
 
-const populatePlaces = () =>{
-  
-  if (seePlacesButton.innerHTML==="See My Places"){
-    seePlacesButton.innerHTML ="Go Back"
-  } else{
-   seePlacesButton.innerHTML ="See My Places"
+const populatePlaces = () => {
+  if (seePlacesButton.innerHTML === "See My Places") {
+    seePlacesButton.innerHTML = "Go Back";
+  } else {
+    seePlacesButton.innerHTML = "See My Places";
     holder.replaceChildren();
-    holder.style.display="none"
-    return
+    holder.style.display = "none";
+    return;
   }
-  
- for (let i=0; i<destinations.length;i++)  {
-    const newCard = document.createElement("div")
-    newCard.classList.add("card") 
-    
-    const newImg = document.createElement("img")
-    newImg.src= destinations[i]
-    const newText = document.createElement("div")
-    newText.classList.add("card-text") 
-    newText.innerHTML = names[i]
-    newCard.appendChild(newImg)
-    newCard.appendChild(newText)
-    holder.appendChild(newCard)
-    holder.style.display="flex"
-  }
-}
 
-seePlacesButton.addEventListener("click", populatePlaces)
+  for (let i = 0; i < destinations.length; i++) {
+    const newCard = document.createElement("div");
+    newCard.classList.add("card");
+
+    const newImg = document.createElement("img");
+    newImg.src = destinations[i];
+    const newText = document.createElement("div");
+    newText.classList.add("card-text");
+    newText.innerHTML = names[i];
+    newCard.appendChild(newImg);
+    newCard.appendChild(newText);
+    holder.appendChild(newCard);
+    holder.style.display = "flex";
+  }
+};
+
+seePlacesButton.addEventListener("click", populatePlaces);
--- script/main.js
+++ script/main.js
@@ -1,24 +1,22 @@
 const quotes = [
-  'If I cannot do great things, I can do small things in a great way.” - Martin Luther King Jr',
-  'To succeed in life, you need three things: a wishbone, a backbone and a funny bone.” - Reba McEntire',
-  'You can’t cross the sea merely by standing and staring at the water.” - Rabindranath Tagore',
+  "If I cannot do great things, I can do small things in a great way.” - Martin Luther King Jr",
+  "To succeed in life, you need three things: a wishbone, a backbone and a funny bone.” - Reba McEntire",
+  "You can’t cross the sea merely by standing and staring at the water.” - Rabindranath Tagore",
   'To the mind that is still, the whole universe surrenders." - Lao Tzu',
-  'If you\'re always trying to be normal, you will never know how amazing you can be." - Maya Angelou',
-  'It\'s never too late to be what you might have been." - George Eliot',
+  "If you're always trying to be normal, you will never know how amazing you can be.\" - Maya Angelou",
+  "It's never too late to be what you might have been.\" - George Eliot",
   'The most authentic thing about us is our capacity to create, to overcome, to endure, to transform, to love and to be greater than our suffering." - Ben Okri',
-  'If you don’t like something, change it. If you can’t change it, change your attitude.” — Maya Angelou',
-  'It always seems impossible until it\'s done.” - Nelson Mandela'
-]
-function getRandomQuote (){
-  const randomIndex = Math.floor(Math.random() * quotes.length); 
-  const quote =  quotes[randomIndex];
+  "If you don’t like something, change it. If you can’t change it, change your attitude.” — Maya Angelou",
+  "It always seems impossible until it's done.” - Nelson Mandela",
+];
+function getRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
   const quoteSpan = document.getElementById("theQuote");
-  quoteSpan.innerHTML= quote;
+  quoteSpan.innerHTML = quote;
 }
 
-setInterval(getRandomQuote,4000)
-
-
+setInterval(getRandomQuote, 4000);
 
 let submitButton = document.getElementById("submit-button");
 function handleClick() {
@@ -28,12 +26,11 @@ function handleClick() {
   let message = "";
   if (fullName !== "" && email !== "" && textBox !== "") {
     message = `Hello, ${fullName}! We received your email address (${email}) and your comment (${textBox}). I'll get back to you soon!`;
-  swal(message);
+    swal(message);
   } else if (fullName !== "" && email !== "" && textBox === "") {
     message = `Hello, ${fullName}, we received your email address (${email}).`;
-  swal(message);
+    swal(message);
   }
 }
 
 submitButton.addEventListener("click", handleClick);
-
